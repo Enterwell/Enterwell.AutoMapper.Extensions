a4eb7488d734bf158a7f9dfa505a3e91a4e85142
Josip Paladin <jo.paladin@gmail.com>
2022-01-28 12:47:13 +0100

    Added mock classes and test automapper profile

---
+++ Enterwell.AutoMapper.Extensions.Tests/MockEntitySource.cs
@@ -0,0 +1,10 @@
+namespace Enterwell.AutoMapper.Extensions.Tests
+{
+    /// <summary>
+    /// Mock entity for source.
+    /// </summary>
+    public class MockEntitySource
+    {
+        public string Prop1 { get; set; }
+    }
+}
