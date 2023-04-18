a3259e941b53788c389d82ba35eec2a2351b591f
Josip Paladin <jo.paladin@gmail.com>
2023-04-18 19:35:22 +0200

    comments

--- Enterwell.AutoMapper.Extensions.Tests/MockSimpleEntitySource.cs
+++ Enterwell.AutoMapper.Extensions.Tests/MockSimpleEntitySource.cs
@@ -3,29 +3,38 @@ using System;
 namespace Enterwell.AutoMapper.Extensions.Tests
 {
     /// <summary>
-    /// Mock entity for source.
+    /// Mock simple entity source
     /// </summary>
     public class MockSimpleEntitySource
     {
         /// <summary>
-        /// Gets or sets the prop1.
+        /// Gets or sets the common property one.
         /// </summary>
         /// <value>
-        /// The prop1.
+        /// The common property one.
         /// </value>
         public string CommonPropOne { get; set; }
 
         /// <summary>
-        /// Gets or sets the property source one.
+        /// Gets or sets the source property one.
         /// </summary>
         /// <value>
-        /// The property source one.
+        /// The source property one.
         /// </value>
         public int SourcePropOne { get; set; }
     }
 
+    /// <summary>
+    /// Mock additional entity source.
+    /// </summary>
     public class MockAdditionalEntitySource
     {
+        /// <summary>
+        /// Gets or sets the additional source property.
+        /// </summary>
+        /// <value>
+        /// The additional source property.
+        /// </value>
         public DateTime AdditionalSourceProperty { get; set; }
     }
 }
